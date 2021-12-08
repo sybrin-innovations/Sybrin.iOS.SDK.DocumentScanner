002ff726ae5adc8c95ced6d72e91f9b7b4367b6c
Armand Riley <armand.riley@sybrin.com>
2021-12-08 12:39:02 +0200

    - added framework file

---
+++ Sybrin_iOS_DocumentScanner.framework/Headers/Sybrin_iOS_DocumentScanner.h
@@ -0,0 +1,18 @@
+//
+//  Sybrin_iOS_DocumentScanner.h
+//  Sybrin.iOS.DocumentScanner
+//
+//  Created by Default on 2021/11/10.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sybrin_iOS_DocumentScanner.
+FOUNDATION_EXPORT double Sybrin_iOS_DocumentScannerVersionNumber;
+
+//! Project version string for Sybrin_iOS_DocumentScanner.
+FOUNDATION_EXPORT const unsigned char Sybrin_iOS_DocumentScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sybrin_iOS_DocumentScanner/PublicHeader.h>
+
+
